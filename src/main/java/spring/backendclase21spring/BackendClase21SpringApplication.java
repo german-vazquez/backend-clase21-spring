defac5bcb81e4b9eafcc975c0c22e9a21fa8b131
German Vazquez <germanalfredovazquez@gmail.com>
2022-09-07 16:42:30 -0300

    Initial commit

---
+++ src/main/java/spring/backendclase21spring/BackendClase21SpringApplication.java
@@ -0,0 +1,22 @@
+package spring.backendclase21spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class BackendClase21SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendClase21SpringApplication.class, args);
+	}
+	@GetMapping
+	public String inicio(){
+
+		return "Hola mundo!";
+	}
+
+}
---
+++ src/test/java/spring/backendclase21spring/BackendClase21SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.backendclase21spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendClase21SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
